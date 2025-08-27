3290e71340867ffe7f359634c09b80ebd7b09add
Maddi <maddi.fitzmaurice@gmail.com>
2025-08-27 11:38:24 +1000

    Functionality finished

--- ReactToDo/src/ToDoList.jsx
+++ ReactToDo/src/ToDoList.jsx
@@ -2,27 +2,42 @@ import React, { useState } from 'react'
 
 export default function ToDoList() {
 
-    const [tasks, setTasks] = useState(["Eat breakfast", "Take a shower", "Walk the dog"]);
-    const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState(["Eat breakfast", "Take a shower", "Walk the dog"]);
+  const [newTask, setNewTask] = useState("");
 
   function handleInputChange(event) {
     setNewTask(event.target.value);
   }
 
   function addTask() {
-
+    if (newTask.trim() !== "") {
+      setTasks(t => [...t, newTask])
+      setNewTask("");
+    }
   }
+    
 
   function deleteTask(index) {
-
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
   }
 
   function moveTaskUp(index) {
 
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    }
   }
 
   function moveTaskDown(index) {
-
+    
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    }
   }
 
 
